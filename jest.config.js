9ce520c73dd3104c512030ba4a9809a18e31b772
datapharmer <6548767+datapharmer@users.noreply.github.com>
2024-11-26 16:36:46 -0500

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 const { jestConfig } = require('@salesforce/sfdx-lwc-jest/config');
 
 module.exports = {
-    format: 'es' // Set to 'es' or 'system' 
     ...jestConfig,
     modulePathIgnorePatterns: ['<rootDir>/.localdevserver']
 };
