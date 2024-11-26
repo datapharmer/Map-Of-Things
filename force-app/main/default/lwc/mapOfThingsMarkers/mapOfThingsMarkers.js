8da4886c264e7629ac1d988e9b365ede54308d32
datapharmer <6548767+datapharmer@users.noreply.github.com>
2024-11-26 12:41:11 -0500

    Update mapOfThingsMarkers.js

--- force-app/main/default/lwc/mapOfThingsMarkers/mapOfThingsMarkers.js
+++ force-app/main/default/lwc/mapOfThingsMarkers/mapOfThingsMarkers.js
@@ -3,7 +3,7 @@ import LEAFLET_JS from '@salesforce/resourceUrl/leafletjs';
 import LEAFLETADDON from '@salesforce/resourceUrl/leafletjs_marker_rotate_addon';
 import LEAFLETCUSTOM from '@salesforce/resourceUrl/leaflet_custom_css';
 //import * as L from "@salesforce/resourceUrl/leaflet-src.esm.js"
-globalThis.L = L
+//globalThis.L = L
 
 
 
