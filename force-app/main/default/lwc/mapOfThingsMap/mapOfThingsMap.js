e7be059ce23e8c27479c3462e3e4e35ec64efb88
datapharmer <6548767+datapharmer@users.noreply.github.com>
2024-11-26 12:48:42 -0500

    Update mapOfThingsMap.js

--- force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
+++ force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
@@ -3,7 +3,6 @@ import { loadScript, loadStyle } from 'lightning/platformResourceLoader';
 import LEAFLET_JS from '@salesforce/resourceUrl/leafletjs';
 import LEAFLETADDON from '@salesforce/resourceUrl/leafletjs_marker_rotate_addon';
 import LEAFLETCUSTOM from '@salesforce/resourceUrl/leaflet_custom_css';
-import * as L from "leaflet";
 
 
 const LEAFLET_CSS_URL = '/leaflet.css';
