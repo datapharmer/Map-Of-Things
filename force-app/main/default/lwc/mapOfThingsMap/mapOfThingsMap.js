2c5438a3688d89b083610caa614262bfdd618e56
datapharmer <6548767+datapharmer@users.noreply.github.com>
2025-01-08 12:02:20 -0500

    uncomment new const references

--- force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
+++ force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
@@ -11,10 +11,10 @@ import SCHOOLDISTRICTS from'@salesforce/resourceUrl/schooldistricts';
 
 const LEAFLET_CSS_URL = '/leaflet.css';
 const LEAFLET_JS_URL = '/leaflet.js';
-//const CATILINE_JS_URL = '/catiline.js';
-//const SHPFILE_JS_URL = '/leaflet.shpfile.js';
-//const SHP_JS_URL = '/shp.js';
-//const SCHOOLDISTRICTS_URL = '/schooldistricts.zip';
+const CATILINE_JS_URL = '/catiline.js';
+const SHPFILE_JS_URL = '/leaflet.shpfile.js';
+const SHP_JS_URL = '/shp.js';
+const SCHOOLDISTRICTS_URL = '/schooldistricts.zip';
 const MIN_ZOOM = 2;
 const FIT_BOUNDS_PADDING = [20, 20];
 const MAP_CONTAINER = 'div.map-container';
