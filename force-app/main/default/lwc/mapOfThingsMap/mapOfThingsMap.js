c675ebd9534fb1fab987e95536e4e8cc94ce4615
datapharmer <6548767+datapharmer@users.noreply.github.com>
2025-01-10 10:59:36 -0500

    add logging connectedcallback

--- force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
+++ force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
@@ -85,9 +85,9 @@ export default class MapOfThingsMap extends LightningElement {
 	    				console.log("start loading shapefile with school districts: " + this.schooldistrictsUrl);
 	    //todo: check into rangeparent issue in firefox related to Component.index():'Invalid redundant use of component.index().
 		        var shpfile = new L.Shapefile(this.schooldistrictsUrl, {
-			console.log("adding shapefile features")
+			connectedCallback() {console.log("adding shapefile features");}
 			onEachFeature: function(feature, layer) {
-				console.log("loading feature: " + feature + " and layer: " + layer)
+				console.log("loading feature: " + feature + " and layer: " + layer);
 				if (feature.properties) {
 					layer.bindPopup(Object.keys(feature.properties).map(function(k) {
 						console.log("k: " + k)
