ea84578435b77ff9ab03f6a24933ddf092e7f068
datapharmer <6548767+datapharmer@users.noreply.github.com>
2025-01-11 16:16:18 -0500

    Update mapOfThingsMap.js

--- force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
+++ force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
@@ -65,8 +65,8 @@ export default class MapOfThingsMap extends LightningElement {
 	    loadScript(this, LEAFLET_JS + LEAFLET_JS_URL),
 	    loadScript(this, LEAFLET_JS + LEAFLETADDON_JS_URL),
             loadScript(this, LEAFLET_JS + CATILINE_JS_URL),
-	    //loadScript(this, LEAFLET_JS + SHP_JS_URL),
-           // loadScript(this, LEAFLET_JS + SHPFILE_JS_URL)
+            loadScript(this, LEAFLET_JS + SHPFILE_JS_URL),
+	    loadScript(this, LEAFLET_JS + SHP_JS_URL)
         ]).then(() => {
 	   console.log("process promise");
             this.drawMap();
