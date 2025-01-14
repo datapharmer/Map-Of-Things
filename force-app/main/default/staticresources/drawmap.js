82e95e6d64f414bb7d9eff634954ae75249fa051
datapharmer <6548767+datapharmer@users.noreply.github.com>
2025-01-14 16:43:48 -0500

    Create drawmap.js

---
+++ force-app/main/default/staticresources/drawmap.js
@@ -0,0 +1,35 @@
+export function drawMap(){
+	    				console.log("start drawing map");
+        const container = this.template.querySelector(MAP_CONTAINER);
+        this.map = L.map(container, { 
+            zoomControl: true, tap:false   
+        }).setView(this.mapDefaultPosition, this.mapDefaultZoomLevel);    
+        L.tileLayer(this.tileServerUrl, {
+            minZoom: MIN_ZOOM,
+            attribution: this.tileServerAttribution,
+            unloadInvisibleTiles: true
+        }).addTo(this.map);
+	    				console.log("start loading shapefile with school districts: " + this.schooldistrictsUrl);
+	    //todo: check into rangeparent issue in firefox related to Component.index():'Invalid redundant use of component.index().
+
+		        var shpfile = new L.Shapefile(this.schooldistrictsUrl, {
+			onEachFeature: function(feature, layer) {
+				if (feature.properties) {
+					layer.bindPopup(Object.keys(feature.properties).map(function(k) {
+						return k + ": " + feature.properties[k];
+					}).join("<br />"), {
+						maxHeight: 200
+					});
+				}
+			}
+		});
+
+		shpfile.addTo(this.map);
+	    		console.log("shapefile data added to map");
+			shpfile.once("data:loaded", function() {
+				console.log("finished loaded shapefile");
+			});
+			this.dispatchEvent(new CustomEvent(
+				CUSTOM_EVENT_INIT, {detail: this.map}
+			));
+    }
