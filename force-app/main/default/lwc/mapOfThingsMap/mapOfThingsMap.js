2b7a54abd18f59613090f0b998dc1675668baee8
datapharmer <6548767+datapharmer@users.noreply.github.com>
2025-01-23 11:11:49 -0500

    remove conversion to base64

--- force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
+++ force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
@@ -119,7 +119,7 @@ export default class MapOfThingsMap extends LightningElement {
 	    				console.log("start loading shapefile with school districts: " + SCHOOLDISTRICTS);
 	    //todo: check into rangeparent issue in firefox related to Component.index():'Invalid redundant use of component.index().
 
-		        var shpfile = new L.Shapefile(this.convertToBase64(SCHOOLDISTRICTS), {
+		        var shpfile = new L.Shapefile(this.SCHOOLDISTRICTS, {
 			onEachFeature: function(feature, layer) {
 				if (feature.properties) {
 					layer.bindPopup(Object.keys(feature.properties).map(function(k) {
