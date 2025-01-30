5b0b823663afe64c6e54218af3783d908f8e9851
datapharmer <6548767+datapharmer@users.noreply.github.com>
2025-01-30 14:46:36 -0500

    Update mapOfThingsMap.js

--- force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
+++ force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
@@ -118,7 +118,7 @@ export default class MapOfThingsMap extends LightningElement {
 	    				console.log("shapefile with school districts details: " + shapedata);
 	    //todo: check into rangeparent issue in firefox related to Component.index():'Invalid redundant use of component.index().
 
-		        var shpfile = new L.Shapefile(shapedata, {
+		        let var shpfile = await new L.Shapefile(shapedata, {
 			onEachFeature: function(feature, layer) {
 				if (feature.properties) {
 					layer.bindPopup(Object.keys(feature.properties).map(function(k) {
