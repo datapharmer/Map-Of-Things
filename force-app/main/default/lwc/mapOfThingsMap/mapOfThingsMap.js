f778af6a52fb988c18bcf0b8df34b069807511bf
datapharmer <6548767+datapharmer@users.noreply.github.com>
2025-01-30 14:50:39 -0500

    narrow down promise issues

--- force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
+++ force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
@@ -118,8 +118,8 @@ export default class MapOfThingsMap extends LightningElement {
 	    				console.log("shapefile with school districts details: " + shapedata);
 	    //todo: check into rangeparent issue in firefox related to Component.index():'Invalid redundant use of component.index().
 
-		        let shpfile = await new L.Shapefile(shapedata, {
-			onEachFeature: function(feature, layer) {
+		        shpfile = new L.Shapefile(shapedata, {
+			/*onEachFeature: function(feature, layer) {
 				if (feature.properties) {
 					layer.bindPopup(Object.keys(feature.properties).map(function(k) {
 						return k + ": " + feature.properties[k];
@@ -127,7 +127,7 @@ export default class MapOfThingsMap extends LightningElement {
 						maxHeight: 200
 					});
 				}
-			}
+			} */
 		}); 
 		console.log("adding shapedata to map via shpfile");
 		//shpfile.addTo(this.map);
