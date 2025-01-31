92ed2e8c244f5c0497530d606de721f19ce61f4f
datapharmer <6548767+datapharmer@users.noreply.github.com>
2025-01-31 04:10:23 -0500

    it's L.Shapefile

--- force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
+++ force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
@@ -130,7 +130,8 @@ export default class MapOfThingsMap extends LightningElement {
 			//} 
 		//});
 
-        var shapefile =  new L.Shapefile();//new L.Shapefile(shapedata); 
+        //var shapefile =  new L.Shapefile();//new L.Shapefile(shapedata); 
+	L.Shapefile(shapedata, {});
 	console.log("shapefile instantiated");
        /* shapefile.then(function(layer) {
              //Add the loaded layer to your map
