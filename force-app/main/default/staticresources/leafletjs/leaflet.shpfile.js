9482a1ff91550c6a32bef2811988b41184786d26
datapharmer <6548767+datapharmer@users.noreply.github.com>
2025-01-31 05:54:33 -0500

    refactor

--- force-app/main/default/staticresources/leafletjs/leaflet.shpfile.js
+++ force-app/main/default/staticresources/leafletjs/leaflet.shpfile.js
@@ -2,62 +2,86 @@
 
 /* global cw, shp */
 L.Shapefile = L.GeoJSON.extend({
-  options: {
-    importUrl: 'shp.js'
-  },
+    options: {
+        importUrl: 'shp.js',
+        isArrayBuffer: false // Corrected typo
+    },
 
-  initialize: function(file, options) {
-    L.Util.setOptions(this, options);
-    if (typeof cw !== 'undefined') {
-      /*eslint-disable no-new-func*/
-      if (!options.isArrayBuffer) {
-        this.worker = cw(new Function('data', 'cb', 'importScripts("' + this.options.importUrl + '");shp(data).then(cb);'));
-      } else {
-        this.worker = cw(new Function('data', 'importScripts("' + this.options.importUrl + '"); return shp.parseZip(data);'));
-      }
-      /*eslint-enable no-new-func*/
-    }
-    L.GeoJSON.prototype.initialize.call(this, {
-      features: []
-    }, options);
-    this.addFileData(file);
-  },
+    initialize: function(file, options) {
+        L.Util.setOptions(this, options);
 
-  addFileData: function(file) {
-    var self = this;
-    this.fire('data:loading');
-    if (typeof file !== 'string' && !('byteLength' in file)) {
-      var data = this.addData(file);
-      this.fire('data:loaded');
-      return data;
-    }
-    if (!this.worker) {
-      shp(file).then(function(data) {
-        self.addData(data);
-        self.fire('data:loaded');
-      }).catch(function(err) {
-        self.fire('data:error', err);
-      })
-      return this;
-    }
-    var promise;
-    if (this.options.isArrayBufer) {
-      promise = this.worker.data(file, [file]);
-    } else {
-      promise = this.worker.data(cw.makeUrl(file));
-    }
+        if (typeof cw !== 'undefined') {
+            if (!options.isArrayBuffer) {
+                this.worker = cw(this.createWorkerFunction(this.options.importUrl, false));
+            } else {
+                this.worker = cw(this.createWorkerFunction(this.options.importUrl, true));
+            }
+        }
 
-    promise.then(function(data) {
-      self.addData(data);
-      self.fire('data:loaded');
-      self.worker.close();
-    }).then(function() {}, function(err) {
-      self.fire('data:error', err);
-    })
-    return this;
-  }
+        L.GeoJSON.prototype.initialize.call(this, {
+            features: []
+        }, options);
+        this.addFileData(file);
+    },
+
+    createWorkerFunction: function(importUrl, isArrayBuffer) {
+        // Create a Blob containing the worker function code
+        const workerCode = `
+            importScripts("${importUrl}");
+            self.onmessage = function(event) {
+                const data = event.data;
+                if (!${isArrayBuffer}) {
+                    shp(data).then(cb => self.postMessage(cb));
+                } else {
+                    self.postMessage(shp.parseZip(data));
+                }
+            };
+        `;
+
+        const blob = new Blob([workerCode], { type: 'application/javascript' });
+        const blobURL = URL.createObjectURL(blob);
+        return blobURL; // Return URL for the worker
+    },
+
+
+    addFileData: function(file) {
+        var self = this;
+        this.fire('data:loading');
+
+        if (typeof file !== 'string' && !('byteLength' in file)) {
+            var data = this.addData(file);
+            this.fire('data:loaded');
+            return data;
+        }
+
+        if (!this.worker) {
+            shp(file).then(function(data) {
+                self.addData(data);
+                self.fire('data:loaded');
+            }).catch(function(err) {
+                self.fire('data:error', err);
+            });
+            return this;
+        }
+
+        var promise;
+        if (this.options.isArrayBuffer) {
+            promise = this.worker.data(file, [file]);
+        } else {
+            promise = this.worker.data(cw.makeUrl(file));
+        }
+
+        promise.then(function(data) {
+            self.addData(data);
+            self.fire('data:loaded');
+            self.worker.close(); // Close the worker
+        }).catch(function(err) { // Corrected .then to .catch
+            self.fire('data:error', err);
+        });
+        return this;
+    }
 });
 
 L.shapefile = function(a, b, c) {
-  return new L.Shapefile(a, b, c);
+    return new L.Shapefile(a, b, c);
 };
