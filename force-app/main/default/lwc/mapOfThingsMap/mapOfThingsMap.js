0262b799f676dad4b2f5576d91e59a977d98d232
datapharmer <6548767+datapharmer@users.noreply.github.com>
2025-02-13 18:43:45 -0500

    Update mapOfThingsMap.js

--- force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
+++ force-app/main/default/lwc/mapOfThingsMap/mapOfThingsMap.js
@@ -6,7 +6,6 @@ import SCHOOLDISTRICTS_ZIP from '@salesforce/resourceUrl/schooldistricts';
 const LEAFLET_CSS_URL = '/leaflet.css';
 const LEAFLET_JS_URL = '/leaflet.js';
 const LEAFLETADDON_JS_URL = '/leafletjs_marker_rotate_addon.js';
-const TURF_JS_URL = '/turf.js';
 const SHPFILE_JS_URL = '/leaflet.shpfile.js';
 const SHP_JS_URL = '/shp.js';
 const CATILINE_JS_URL = '/catiline.js';
@@ -63,11 +62,9 @@ export default class MapOfThingsMap extends LightningElement {
                 loadStyle(this, LEAFLET_JS + LEAFLET_CSS_URL),
                 loadScript(this, LEAFLET_JS + LEAFLET_JS_URL),
                 loadScript(this, LEAFLET_JS + LEAFLETADDON_JS_URL),
-                loadScript(this, LEAFLET_JS + TURF_JS_URL),                
                 loadScript(this, LEAFLET_JS + CATILINE_JS_URL),
                 loadScript(this, LEAFLET_JS + SHP_JS_URL),
-                loadScript(this, LEAFLET_JS + SHPFILE_JS_URL),
-                loadScript(this, TURF_JS) // Load turf.js
+                loadScript(this, LEAFLET_JS + SHPFILE_JS_URL)
             ]);
             this.drawMap();
         } catch (error) {
@@ -89,49 +86,11 @@ export default class MapOfThingsMap extends LightningElement {
             unloadInvisibleTiles: true
         }).addTo(this.map);
 
-        // Render markers if they exist
-        if (this.markersExist) {
-            this.renderMarkers();
-        }
-
         // Render shapefile
         await this.renderShapefile();
 
         // Dispatch custom event to notify the map is initialized
         this.dispatchEvent(new CustomEvent(CUSTOM_EVENT_INIT, { detail: this.map }));
-    }
-
-    renderMarkers() {
-        // Clear existing markers
-        if (this.markerLayer) {
-            this.map.removeLayer(this.markerLayer);
-        }
-
-        // Define custom icon for the markers
-        const customIcon = L.icon({
-            iconUrl: 'https://www.trustindiana.in.gov/wp-content/uploads/2018/06/School-Icon-300x300@2x.png', // Custom icon URL
-            iconSize: [50, 50], // Adjust the size of the icon as needed
-            iconAnchor: [25, 50] // Anchor point to properly position the icon on the map
-        });
-
-        // Create a layer group for the markers
-        this.markerLayer = L.layerGroup(
-            this.markers.map(marker => {
-                return L.marker([marker.lat, marker.lng], {
-                    icon: customIcon, // Use the custom icon
-                    title: marker.title || '',
-                    rotationAngle: marker.rotationAngle || 0 // Optional: if using the marker rotation addon
-                }).bindPopup(marker.popupContent || '');
-            })
-        );
-
-        // Add the marker layer to the map
-        this.markerLayer.addTo(this.map);
-
-        // Auto fit bounds if enabled
-        if (this.autoFitBounds && this.markersExist) {
-            this.map.flyToBounds(this.bounds, { padding: FIT_BOUNDS_PADDING });
-        }
     }
 
     async renderShapefile() {
@@ -147,37 +106,14 @@ export default class MapOfThingsMap extends LightningElement {
             const arrayBuffer = await response.arrayBuffer();
             const geojson = await shp(arrayBuffer); // Use `shp.js` to parse the zip file into GeoJSON
 
-            // Filter GeoJSON features to only include those that intersect with any marker
-            const filteredFeatures = geojson.features.filter(feature => {
-                const polygon = turf.polygon(feature.geometry.coordinates);
-                return this.markers.some(marker => {
-                    const point = turf.point([marker.lng, marker.lat]);
-                    return turf.booleanIntersects(polygon, point);
-                });
-            });
-
-            // Create a new GeoJSON object with the filtered features
-            const filteredGeoJSON = {
-                type: 'FeatureCollection',
-                features: filteredFeatures
-            };
-
-            // Function to generate a random color
-            function getRandomColor() {
-                const letters = '0123456789ABCDEF';
-                let color = '#';
-                for (let i = 0; i < 6; i++) {
-                    color += letters[Math.floor(Math.random() * 16)];
-                }
-                return color;
-            }
+            // Filter GeoJSON features to include only those containing markers
+            const filteredGeojson = this.filterGeojsonFeaturesByMarkers(geojson);
 
             // Add filtered GeoJSON to the map with styles
-            const geoJsonLayer = L.geoJSON(filteredGeoJSON, {
+            const geoJsonLayer = L.geoJSON(filteredGeojson, {
                 style: function(feature) {
                     return {
                         color: '#CC5500',
-                        //color: getRandomColor(), // Assign a random color to each feature
                         weight: 2,
                         opacity: 1,
                         fillOpacity: 0.5 // Adjust fill opacity for visibility
@@ -190,7 +126,7 @@ export default class MapOfThingsMap extends LightningElement {
                 }
             }).addTo(this.map);
 
-            // Fit map bounds to filtered GeoJSON
+            // Fit map bounds to GeoJSON
             if (this.autoFitBounds) {
                 const bounds = geoJsonLayer.getBounds();
                 if (bounds.isValid()) {
@@ -200,6 +136,26 @@ export default class MapOfThingsMap extends LightningElement {
         } catch (error) {
             console.error('Error loading or parsing shapefile:', error);
         }
+    }
+
+    filterGeojsonFeaturesByMarkers(geojson) {
+        if (!this.markersExist) {
+            return { type: 'FeatureCollection', features: [] };
+        }
+
+        const markersLatLng = this.markers.map(marker => L.latLng(marker.lat, marker.lng));
+
+        const filteredFeatures = geojson.features.filter(feature => {
+            const shapeBounds = L.geoJSON(feature).getBounds();
+
+            // Check if any marker is within the bounds of the current shape
+            return markersLatLng.some(markerLatLng => shapeBounds.contains(markerLatLng));
+        });
+
+        return {
+            type: 'FeatureCollection',
+            features: filteredFeatures
+        };
     }
 
     generatePopupContent(properties) {
