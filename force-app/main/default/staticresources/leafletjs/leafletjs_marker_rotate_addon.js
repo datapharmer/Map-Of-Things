95d65fcd9a4c6cdb1dc2b49a82225fd23b13b1b4
datapharmer <6548767+datapharmer@users.noreply.github.com>
2025-03-31 14:57:26 -0400

    another attempt to fix rotate addon for mozilla....

--- force-app/main/default/staticresources/leafletjs/leafletjs_marker_rotate_addon.js
+++ force-app/main/default/staticresources/leafletjs/leafletjs_marker_rotate_addon.js
@@ -1,64 +1,81 @@
-/*
- * Firefox-compatible Leaflet Marker Rotation Addon for LWC
- * Handles PointerEvents and LWS constraints
- */
-(function() {
-    function initRotateAddon(L) {
-        if (!L || !L.Marker) return;
+(function () {
+  // Only run if Leaflet is defined
+  if (typeof L === 'undefined') return;
+  
+  var originalSetPos = L.Marker.prototype._setPos;
+  
+  L.Marker.include({
+    _updateImg: function(element, anchor, size) {
+      if (!element || !element.style) return;
+      
+      // Use a safer approach to calculate points
+      var anchorPoint = L.point(anchor || [0, 0]);
+      var sizePoint = L.point(size || [0, 0]);
+      var offset = sizePoint.divideBy(2)._subtract(anchorPoint);
+      
+      // Build transform string safely
+      var transforms = [];
+      transforms.push('translate(' + (-offset.x) + 'px, ' + (-offset.y) + 'px)');
+      
+      if (this.options.iconAngle) {
+        transforms.push('rotate(' + this.options.iconAngle + 'deg)');
+      }
+      
+      transforms.push('translate(' + offset.x + 'px, ' + offset.y + 'px)');
+      
+      // Apply transform in a more browser-compatible way
+      try {
+        element.style.transform = transforms.join(' ');
+      } catch (e) {
+        console.warn('Failed to set transform:', e);
+      }
+    },
 
-        var _oldSetPos = L.Marker.prototype._setPos;
-        var _oldUpdate = L.Marker.prototype.update;
+    setIconAngle: function (iconAngle) {
+      this.options.iconAngle = iconAngle;
+      if (this._map) this.update();
+      return this;
+    },
 
-        L.Marker.include({
-            _updateTransform: function() {
-                if (!this._icon || !this.options.iconAngle) return;
-
-                var icon = this.options.icon || new L.Icon.Default();
-                var anchor = icon.options.iconAnchor || [12, 41];
-                var size = icon.options.iconSize || [25, 41];
-                var center = L.point(size).divideBy(2)._subtract(L.point(anchor));
-
-                var transform = [
-                    'translate(' + -center.x + 'px, ' + -center.y + 'px)',
-                    'rotate(' + this.options.iconAngle + 'deg)',
-                    'translate(' + center.x + 'px, ' + center.y + 'px)'
-                ].join(' ');
-
-                L.DomUtil.setStyle(this._icon, 'transform', transform);
-                L.DomUtil.setStyle(this._icon, '-webkit-transform', transform);
-                L.DomUtil.setStyle(this._icon, '-ms-transform', transform);
-            },
-
-            setIconAngle: function(angle) {
-                this.options.iconAngle = angle;
-                if (this._map) {
-                    this._updateTransform();
-                }
-                return this;
-            },
-
-            _setPos: function(pos) {
-                _oldSetPos.call(this, pos);
-                this._updateTransform();
-            },
-
-            update: function() {
-                _oldUpdate.call(this);
-                this._updateTransform();
-                return this;
-            }
-        });
+    _setPos: function (pos) {
+      // Clear transforms first to prevent accumulation
+      if (this._icon && this._icon.style) {
+        this._icon.style.transform = '';
+      }
+      
+      if (this._shadow && this._shadow.style) {
+        this._shadow.style.transform = '';
+      }
+      
+      // Call original implementation
+      originalSetPos.call(this, pos);
+      
+      // Apply rotation if specified
+      if (this.options.iconAngle) {
+        try {
+          var defaultIcon = new L.Icon.Default();
+          var anchor = this.options.icon && this.options.icon.options.iconAnchor || 
+                      defaultIcon.options.iconAnchor;
+          var size = this.options.icon && this.options.icon.options.iconSize || 
+                    defaultIcon.options.iconSize;
+          
+          if (this._icon) {
+            this._updateImg(this._icon, anchor, size);
+          }
+          
+          if (this._shadow) {
+            var shadowAnchor = this.options.icon && this.options.icon.options.shadowAnchor || 
+                              (this.options.icon && this.options.icon.options.iconAnchor) ||
+                              defaultIcon.options.shadowAnchor;
+            var shadowSize = this.options.icon && this.options.icon.options.shadowSize || 
+                            defaultIcon.options.shadowSize;
+            
+            this._updateImg(this._shadow, shadowAnchor, shadowSize);
+          }
+        } catch (e) {
+          console.warn('Error applying marker rotation:', e);
+        }
+      }
     }
-
-    // Handle different loading scenarios
-    if (typeof L !== 'undefined') {
-        initRotateAddon(L);
-    } else {
-        var checkL = setInterval(function() {
-            if (typeof L !== 'undefined') {
-                clearInterval(checkL);
-                initRotateAddon(L);
-            }
-        }, 100);
-    }
+  });
 })();
